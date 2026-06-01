31a4e442f8a22c0e2510d9692c06bc0537170cbe
sipun05 <sipunkumarpanda.05@gmail.com>
2026-06-01 22:23:21 +0530

    Minor changes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+console.log("JWT_SECRET =");
 
 // Register
 router.post('/register', async (req, res) => {
